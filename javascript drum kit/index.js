5145ca9ea515dbfe309c62da1d8a73fb1ea374ca
gauravraj22 <105232271+gauravraj22@users.noreply.github.com>
2022-05-27 22:22:37 +0530

    Update index.js

--- javascript drum kit/index.js
+++ javascript drum kit/index.js
@@ -8,6 +8,87 @@ var audio7 = new Audio("sounds/snare.wav");
 var audio8 = new Audio("sounds/tink.wav");
 var audio9 = new Audio("sounds/tom.wav");
 
-function play(audio) {
+function play(audio){
     audio.play();
+    audio.currentTime=0;
 }
+var animate = document.getElementById("aa");
+animate.addEventListener("click", () => {
+    animate.classList.add("playing")
+
+    setInterval(() => {
+        animate.classList.remove("playing")
+    },200);
+})
+
+var ani = document.getElementById("ss");
+ani.addEventListener("click", () => {
+    ani.classList.add("playing")
+
+    setInterval(() => {
+        ani.classList.remove("playing")
+    },200);
+})
+
+var anim = document.getElementById("dd");
+anim.addEventListener("click", () => {
+    anim.classList.add("playing")
+
+    setInterval(() => {
+        anim.classList.remove("playing")
+    },200);
+})
+
+var anima = document.getElementById("ff");
+anima.addEventListener("click", () => {
+    anima.classList.add("playing")
+
+    setInterval(() => {
+        anima.classList.remove("playing")
+    },200);
+})
+
+var transition = document.getElementById("gg");
+transition.addEventListener("click", () => {
+    transition.classList.add("playing")
+
+    setInterval(() => {
+        transition.classList.remove("playing")
+    },200);
+})
+
+var animat = document.getElementById("hh");
+animat.addEventListener("click", () => {
+    animat.classList.add("playing")
+
+    setInterval(() => {
+        animat.classList.remove("playing")
+    },200);
+})
+
+var transitio = document.getElementById("jj");
+transitio.addEventListener("click", () => {
+    transitio.classList.add("playing")
+
+    setInterval(() => {
+        transitio.classList.remove("playing")
+    },200);
+})
+
+var anima = document.getElementById("kk");
+anima.addEventListener("click", () => {
+    anima.classList.add("playing")
+
+    setInterval(() => {
+        anima.classList.remove("playing")
+    },200);
+})
+
+var trans = document.getElementById("ll");
+trans.addEventListener("click", () => {
+    trans.classList.add("playing")
+
+    setInterval(() => {
+        trans.classList.remove("playing")
+    },200);
+})
